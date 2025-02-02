ea51a9e9731e16896db085662c666eafbccf169f
benzy78 <westvillage815@gmail.com>
2025-02-02 16:34:38 +0900

    細かい修正

--- html/js/html-script.js
+++ html/js/html-script.js
@@ -24,3 +24,21 @@ $(document).ready(function () {
   });
   $(window).trigger('scroll');
 });
+
+// headerのハンバーガーメニューの開閉
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".header__menu-toggle");
+  const navLinks = document.querySelector(".header__nav");
+  const navItems = document.querySelectorAll(".header__nav-links a"); 
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+  navItems.forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active"); 
+    });
+  });
+});
+// headerのハンバーガーメニューの開閉 end
